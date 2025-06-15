693f51bbbf77d6607cff69f635356751809fe311
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-06-15 19:06:44 +0800

    change icon for membership

--- agricart-admin/resources/js/components/app-sidebar.tsx
+++ agricart-admin/resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutDashboard, Package } from 'lucide-react';
+import { BookOpen, Folder, LayoutDashboard, Package, UsersRound } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -21,7 +21,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Membership',
         href: '/membership',
-        icon: Package,
+        icon: UsersRound,
     },
     
 ];
