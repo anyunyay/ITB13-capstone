050778e4095405db1fe4b7785b47d7468baefbef
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-06-24 18:10:30 +0800

    added line of codes for "Direct Relation" (meaning Stock can now access other models data)

--- agricart-admin/app/Models/Stock.php
+++ agricart-admin/app/Models/Stock.php
@@ -10,4 +10,14 @@ class Stock extends Model
     use HasFactory;
 
     protected $fillable = ['quantity'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
