0b12b61b17d6ad6ec70df83286389206199471c1
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-06-26 21:57:33 +0800

    removed and changed migration file for consistency

--- agricart-admin/database/migrations/2025_06_14_120452_create_products_table.php
+++ agricart-admin/database/migrations/2025_06_14_120452_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->decimal('price', 10, 2);
             $table->text('description')->nullable();
             $table->string('image')->nullable();
+            $table->timestamp('archived_at')->nullable();
             $table->timestamps();
         });
     }
