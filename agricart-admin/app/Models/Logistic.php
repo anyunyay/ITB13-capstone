5f36699f47ed76d5dd8e0c8e8d472bf7fea1e63e
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-07-05 18:35:54 +0800

    added logistic for Database

---
+++ agricart-admin/app/Models/Logistic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Logistic extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+    ];
+
+    /** @use HasFactory<\Database\Factories\LogisticFactory> */
+    use HasFactory;
+}
