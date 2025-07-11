60060c64c1d3eb0c5e0069914a6e596792122e96
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-07-11 14:39:24 +0800

    added order in sidebar

--- agricart-admin/resources/js/components/app-sidebar.tsx
+++ agricart-admin/resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutDashboard, Package, UsersRound, IdCard } from 'lucide-react';
+import { BookOpen, Folder, LayoutDashboard, Package, UsersRound, IdCard, ClipboardPen } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -17,6 +17,11 @@ const mainNavItems: NavItem[] = [
         title: 'Inventory',
         href: '/inventory',
         icon: Package,
+    },
+    {
+        title: 'Orders',
+        href: '/orders',
+        icon: ClipboardPen,
     },
     {
         title: 'Membership',
