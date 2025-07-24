0ffd91d80a2bba3531a4ac503a8b6c34d1ede580
Quipit, Mark Aldrin M. <190807227+Mark-Quipit@users.noreply.github.com>
2025-07-24 16:52:09 +0800

    added quantity

--- agricart-admin/database/migrations/2025_07_23_083251_create_cart_items_table.php
+++ agricart-admin/database/migrations/2025_07_23_083251_create_cart_items_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('cart_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->integer('quantity')->default(1);
+            $table->string('category');
+            $table->decimal('quantity', 10, 2);
             $table->timestamps();
         });
     }
