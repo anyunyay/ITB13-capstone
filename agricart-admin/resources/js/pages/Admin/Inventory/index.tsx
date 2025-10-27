bd1dd45f917b0c07afdb4219bb9d90f7fdd3a80b
Mark-Quipit <quipitmarkaldrin81@gmail.com>
2025-10-27 17:27:06 +0800

    updated gap

--- agricart-admin/resources/js/pages/Admin/Inventory/index.tsx
+++ agricart-admin/resources/js/pages/Admin/Inventory/index.tsx
@@ -317,7 +317,7 @@ export default function InventoryIndex() {
             <AppLayout>
                 <Head title="Inventory Management" />
                 <div className="min-h-screen bg-background">
-                    <div className="w-full px-4 py-4 flex flex-col gap-6 sm:px-6 lg:px-8">
+                    <div className="w-full px-4 py-4 flex flex-col gap-2 sm:px-6 lg:px-8">
                         <DashboardHeader stockStats={stockStats} />
 
                         {/* Flash Messages and Alerts */}
