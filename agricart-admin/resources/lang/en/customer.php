af9159989b4e620f5f45c8a298c4c4a09b2d71f0
Mark-Quipit <quipitmarkaldrin81@gmail.com>
2025-11-09 02:16:02 +0800

    updated add to cart modal to be reponsive and have translation

--- agricart-admin/resources/lang/en/customer.php
+++ agricart-admin/resources/lang/en/customer.php
@@ -18,5 +18,21 @@ return [
     'add_to_cart' => 'Add to Cart',
     'out_of_stock' => 'Out of Stock',
     'in_stock' => 'In Stock',
+    
+    // Add to Cart Modal
+    'prices' => 'Prices',
+    'kilo' => 'Kilo',
+    'piece' => 'Piece',
+    'tali' => 'Tali',
+    'stock' => 'Stock',
+    'no_stock' => 'No stock',
+    'quantity' => 'Quantity',
+    'max' => 'Max',
+    'cancel' => 'Cancel',
+    'adding' => 'Adding...',
+    'no_image' => 'No Image',
+    'please_select_category' => 'Please select a category.',
+    'successfully_added_to_cart' => 'Successfully added to cart!',
+    'failed_to_add_to_cart' => 'Failed to add to cart. Please try again.',
 ];
 
