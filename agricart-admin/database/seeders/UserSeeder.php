b80b386060783c1aca5743271324ebaab185da1d
Tanya Leanne Eti <anyaleannee@gmail.com>
2025-11-20 07:22:01 +0800

    added more user data

--- agricart-admin/database/seeders/UserSeeder.php
+++ agricart-admin/database/seeders/UserSeeder.php
@@ -81,6 +81,65 @@ class UserSeeder extends Seeder
             'is_active' => true,
         ]);
 
+        // Additional Logistics User - Elmo V. Republica
+        $logisticUser3 = User::create([
+            'type' => 'logistic',
+            'name' => 'Elmo V. Republica',
+            'email' => 'elmo@logistic.com',
+            'contact_number' => '09111222333',
+            'registration_date' => now()->subDays(20),
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+            'active' => true,
+        ]);
+
+        UserAddress::create([
+            'user_id' => $logisticUser3->id,
+            'street' => '321 Transport Avenue',
+            'barangay' => 'Sala',
+            'city' => 'Cabuyao',
+            'province' => 'Laguna',
+            'is_active' => true,
+        ]);
+
+        // Create Farmer Users
+        $farmers = [
+            ['name' => 'Sonia Canceran', 'email' => 'sonia.canceran@farmer.com'],
+            ['name' => 'Aurora P. Cervantes', 'email' => 'aurora.cervantes@farmer.com'],
+            ['name' => 'Roger Dubos', 'email' => 'roger.dubos@farmer.com'],
+            ['name' => 'Jouie P. Asido', 'email' => 'jouie.asido@farmer.com'],
+            ['name' => 'Ronnie Asido', 'email' => 'ronnie.asido@farmer.com'],
+            ['name' => 'Ronaldo L. Comite', 'email' => 'ronaldo.comite@farmer.com'],
+            ['name' => 'Sotelia B. Cariod', 'email' => 'sotelia.cariod@farmer.com'],
+            ['name' => 'Jell O. Federis', 'email' => 'jell.federis@farmer.com'],
+            ['name' => 'Gregorio L. Bando', 'email' => 'gregorio.bando@farmer.com'],
+            ['name' => 'Noel L. Villare', 'email' => 'noel.villare@farmer.com'],
+            ['name' => 'Jimmy F. Santiago', 'email' => 'jimmy.santiago@farmer.com'],
+            ['name' => 'Cristina R. Rogel', 'email' => 'cristina.rogel@farmer.com'],
+        ];
+
+        foreach ($farmers as $index => $farmerData) {
+            $farmer = User::create([
+                'type' => 'member',
+                'name' => $farmerData['name'],
+                'email' => $farmerData['email'],
+                'contact_number' => '0912' . str_pad($index + 1, 7, '0', STR_PAD_LEFT),
+                'registration_date' => now()->subDays(60 - $index * 2),
+                'password' => Hash::make('12345678'),
+                'email_verified_at' => now(),
+                'active' => true,
+            ]);
+
+            UserAddress::create([
+                'user_id' => $farmer->id,
+                'street' => 'Farm ' . ($index + 1) . ', Agricultural Zone',
+                'barangay' => 'Sala',
+                'city' => 'Cabuyao',
+                'province' => 'Laguna',
+                'is_active' => true,
+            ]);
+        }
+
         // Create one test customer
         User::factory()->count(1)->customer()->create();
 
