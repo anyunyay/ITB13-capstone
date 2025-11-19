33cf344f3938149c7195842b06bdea55f2bd6068
Tanya Leanne Eti <anyaleannee@gmail.com>
2025-11-20 01:22:40 +0800

    updated app logo into SMMC Logo

--- agricart-admin/resources/js/components/shared/layout/app-logo.tsx
+++ agricart-admin/resources/js/components/shared/layout/app-logo.tsx
@@ -1,13 +1,18 @@
-import AppLogoIcon from '@/components/shared/layout/app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-12 items-center justify-center">
+                <img 
+                    src="/storage/logo/SMMC Logo-1.webp" 
+                    alt="SMMC Logo" 
+                    className="size-9 object-contain"
+                    onError={(e) => {
+                        e.currentTarget.src = '/storage/logo/SMMC Logo-1.png';
+                    }}
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">AgriCart</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold">SMMC</span>
             </div>
         </>
     );
