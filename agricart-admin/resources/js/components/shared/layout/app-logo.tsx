6f283ba9ea422aed2034006f1e1497174a11309f
Tanya Leanne Eti <anyaleannee@gmail.com>
2025-11-20 01:43:31 +0800

    updated logo

--- agricart-admin/resources/js/components/shared/layout/app-logo.tsx
+++ agricart-admin/resources/js/components/shared/layout/app-logo.tsx
@@ -12,7 +12,7 @@ export default function AppLogo() {
                 />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">SMMC</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold text-white">SMMC</span>
             </div>
         </>
     );
