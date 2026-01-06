2074ab82c3957a42fa823dc0c8e41ad1c7bee87a
Quipit, Mark Aldrin M. <quipitmarkaldrin81@gmail.com>
2026-01-06 17:44:14 +0800

    updated tailwind for deployment

--- agricart-admin/vite.config.ts
+++ agricart-admin/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/js/app.tsx'],
             refresh: true,
